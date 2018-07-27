eea7e2d67fe881e6fc99214a1dea04b2754e3aa5
Whitney Schaefer <whitney@codeforamerica.org>
2018-07-26 17:27:41 -0700

    add the entire gcf styleguide

---
+++ app/helpers/cfa/styleguide/styleguide_helper.rb
@@ -0,0 +1,25 @@
+module Cfa
+  module Styleguide
+    module StyleguideHelper
+      def styleguide_example
+        content_tag :div, class: 'pattern' do
+          content_tag :div, class: 'pattern__example' do
+            yield
+          end
+        end
+      end
+
+      def status_icon(icon, successful: false, failure: false, not_applicable: false)
+        classes = ["status"]
+        classes << "successful" if successful
+        classes << "failure" if failure
+        classes << "not-applicable" if not_applicable
+        classes << "icon-" + icon
+
+        <<-HTML.html_safe
+      <i class="#{classes.join(' ')}" aria-hidden='true'></i>
+        HTML
+      end
+    end
+  end
+end
