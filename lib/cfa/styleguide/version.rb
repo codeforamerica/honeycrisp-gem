7ed261dbd8762c23bbc151c4744561cc35bffb57
Christa Hartsock <christa@codeforamerica.org>
2018-10-11 17:22:36 -0700

    Fix styling on non-main styleguide pages
    They previously defined all of their own HTML. Now we have them all
    inserted into the base layout, similar to the main page.
    
    This commit also precompiles the cfa styleguide assets, and only
    includes those stylesheets and javascript + Prism on the styleguide
    pages.

--- lib/cfa/styleguide/version.rb
+++ lib/cfa/styleguide/version.rb
@@ -1,5 +1,5 @@
 module Cfa
   module Styleguide
-    VERSION = "0.4.1"
+    VERSION = "0.4.2"
   end
 end
--- spec/feature/pages_spec.rb
+++ spec/feature/pages_spec.rb
@@ -14,4 +14,32 @@ feature 'Pages' do
     expect(page.status_code).to eq 200
     expect(page).to have_content('Atoms')
   end
+
+  scenario 'can load styleguide cbo dashboard' do
+    visit '/cfa/styleguide/cbo-dashboard'
+
+    expect(page.status_code).to eq 200
+    expect(page).to have_content('Assister dashboard')
+  end
+
+  scenario 'can load styleguide cbo analytics' do
+    visit '/cfa/styleguide/cbo-analytics'
+
+    expect(page.status_code).to eq 200
+    expect(page).to have_content('Overall numbers')
+  end
+
+  scenario 'can load styleguide current' do
+    visit '/cfa/styleguide/current'
+
+    expect(page.status_code).to eq 200
+    expect(page).to have_content('The legal stuff')
+  end
+
+  scenario 'can load styleguide custom docs' do
+    visit '/cfa/styleguide/custom-docs'
+
+    expect(page.status_code).to eq 200
+    expect(page).to have_content('First, we’ll need to figure out what proof you need to submit.')
+  end
 end
