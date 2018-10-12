8afa818feb89aee8e7ae279e34c588410f224c59
Christa Hartsock <christa@codeforamerica.org>
2018-10-11 17:22:36 -0700

    Fix styling on non-main styleguide pages
    They previously defined all of their own HTML. Now we have them all
    inserted into the base layout, similar to the main page.
    
    This commit also precompiles the cfa styleguide assets, and only
    includes those stylesheets and javascript + Prism on the styleguide
    pages.

--- lib/cfa/styleguide/engine.rb
+++ lib/cfa/styleguide/engine.rb
@@ -6,7 +6,12 @@ module Cfa
       isolate_namespace Cfa::Styleguide
 
       initializer "cfa-styleguide.assets.precompile" do |app|
-        app.config.assets.precompile += %w( prism.js prism.css )
+        app.config.assets.precompile += %w(
+          cfa_styleguide_main.css
+          cfa_styleguide_main.js
+          prism.js
+          prism.css
+        )
       end
 
       config.generators do |g|
