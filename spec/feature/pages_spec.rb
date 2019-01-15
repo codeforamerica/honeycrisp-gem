c2c507e9bc8fc6c2ed1f3bfc4ae1e2a454259e66
Christa Hartsock <christa@codeforamerica.org>
2019-01-15 15:55:38 -0800

    Add version number to page title
    Addresses #56
    
    Signed-off-by: Whitney Schaefer <whitney@codeforamerica.org>
    Co-authored-by: Whitney Schaefer <whitney@codeforamerica.org>

--- spec/feature/pages_spec.rb
+++ spec/feature/pages_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
 
 feature 'Pages' do
-  scenario 'can load root page' do
-    visit root_path
-
-    expect(page.status_code).to eq 200
-    expect(page).to have_content('CfA Styleguide')
-  end
-
   scenario 'can load styleguide' do
     visit '/cfa/styleguide'
 
     expect(page.status_code).to eq 200
-    expect(page).to have_content('Atoms')
+    expect(page).to have_content("CfA Styleguide v#{Cfa::Styleguide::VERSION}")
   end
 
   scenario 'can load styleguide cbo dashboard' do
