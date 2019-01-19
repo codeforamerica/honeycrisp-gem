7808a04e3eac518cd299f900adeabe4b1aa0adef
Anule Ndukwu <anule@codeforamerica.org>
2019-01-18 17:29:57 -0800

    WE HAVE EMOJI!

--- app/controllers/cfa/styleguide/pages_controller.rb
+++ app/controllers/cfa/styleguide/pages_controller.rb
@@ -7,6 +7,15 @@ module Cfa
         @form = Cfa::Styleguide::FormExample.new
         @form.valid?
       end
+
+      def emojis
+        @emojis = Dir.chdir(File.expand_path('../../../assets/images', File.dirname(__FILE__))) do
+          Dir.glob('emojis/*')
+        end
+        @emoji_pairs = Dir.chdir(File.expand_path('../../../assets/images/', File.dirname(__FILE__))) do
+          Dir.glob('emoji_pairs/*')
+        end
+      end
     end
   end
 end
