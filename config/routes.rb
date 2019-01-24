57a714edf4d76035e6bd172cc9d66639c902a745
Christa Hartsock <christa@codeforamerica.org>
2019-01-23 17:30:49 -0800

    Remove unused styleguide pages
    Also:
    * Adds form builder to nav
    * Extracts header into main, so we can use as consistent header in
    future.
    
    [Finishes #65]
    [Finishes #58]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Cfa::Styleguide::Engine.routes.draw do
   get '/styleguide' => 'pages#index', as: :styleguide_main
-  get '/styleguide/cbo-dashboard' => 'pages#cbo_dashboard', as: :styleguide_cbo_dashboard
-  get '/styleguide/cbo-analytics' => 'pages#cbo_analytics', as: :styleguide_cbo_analytics
-  get '/styleguide/current' => 'pages#current', as: :styleguide_current
-  get '/styleguide/custom-docs' => 'pages#custom_docs', as: :styleguide_custom_docs
   get '/styleguide/form-builder' => 'pages#form_builder', as: :styleguide_form_builder
   get '/styleguide/examples/*example_path' => 'examples#show', as: :styleguide_example
 end
