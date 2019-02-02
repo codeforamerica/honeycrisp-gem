626f2bbb29136959dadd9ecb1c0d1aa8a7eab6bc
Ben Sheldon <bensheldon@gmail.com>
2019-02-01 16:40:14 -0800

    Add form record to examples rendering context

--- app/controllers/cfa/styleguide/examples_controller.rb
+++ app/controllers/cfa/styleguide/examples_controller.rb
@@ -4,6 +4,9 @@ module Cfa
       layout false
 
       def show
+        @form = Cfa::Styleguide::FormExample.new
+        @form.valid?
+        
         @partial_path = "examples/#{params[:example_path]}"
       end
     end
