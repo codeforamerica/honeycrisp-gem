d83a81fa8094dc9360cbff9b97bf66733f8532e3
Ben Sheldon <bensheldon@gmail.com>
2019-02-02 06:57:43 -0800

    Override specific accessibility checks per example

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -1,23 +1,33 @@
 require 'spec_helper'
 
+GENERAL_ACCESSIBILITY_SKIPS = [
+  'bypass',
+  'document-title',
+  'html-has-lang',
+  'landmark-one-main',
+  'page-has-heading-one',
+  'region',
+]
+
+SPECIFIC_ACCESSIBILITY_SKIPS = {
+  'atoms/form_elements' => [
+    'label', # Example demonstrates various input atoms, not label+input molecule
+  ],
+}
+
 RSpec.describe 'Acessibility', js: true do
-  describe 'examples' do
+  describe 'example' do
     example_files = Dir.glob File.expand_path('../../app/views/examples/**/*.html.erb', __dir__)
 
     example_files.each do |example_file|
-      example_filepath = example_file.match(/app\/views\/(.*)\.html.erb/)[1]
+      example_filepath = example_file.match(/app\/views\/examples\/(.*)\.html.erb/)[1]
       example_path = File.dirname(example_filepath) + "/" + File.basename(example_filepath).sub(/^_/, '')
 
       it "is valid - #{example_path}" do
-        visit "/cfa/styleguide/#{example_path}"
-        expect(page).to be_accessible.skipping(
-          'bypass',
-          'document-title',
-          'html-has-lang',
-          'landmark-one-main',
-          'page-has-heading-one',
-          'region'
-        )
+        visit "/cfa/styleguide/examples/#{example_path}"
+
+        accessibility_skips = GENERAL_ACCESSIBILITY_SKIPS + SPECIFIC_ACCESSIBILITY_SKIPS.fetch(example_path, [])
+        expect(page).to be_accessible.skipping(*accessibility_skips)
       end
     end
   end
