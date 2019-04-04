9d4d8d5bc6ad5623fb10f18a9d687b6f4450cac0
Anule Ndukwu <anule@codeforamerica.org>
2019-04-04 14:36:21 -0700

    Refactor all templates into partials
    This restricts the scope of the props being passed down to
    the partials on the page and nothing more.
    
    This format also is accessible from any application.
    To use, add the following lines to your appliciation.html.erb, or
    whatever the base layout file is in your application:
    ```erb
    <div class="template--<%= content_for(:template_name) if content_for?(:template_name) %>">
      <%= content_for(:content) ? content_for(:content) : yield %>
    </div>
    ```

--- app/controllers/cfa/styleguide/layouts_controller.rb
+++ app/controllers/cfa/styleguide/layouts_controller.rb
@@ -21,17 +21,14 @@ module Cfa
       end
 
       def progress_signpost
-        @current_step = 3
-        @step_count = 5
       end
 
       def review_signpost
-        @info_card_type = 'review'
-        @content_for_review = '600' # Save data for review with this instance variable
+        @rent_for_review = '600' # Save data for review with this instance variable
+        @utilities_for_review = '50'
       end
 
       def graphic_signpost
-        @info_card_type = 'graphic'
         @income = '3000'
         @status = 'ineligible'
       end
