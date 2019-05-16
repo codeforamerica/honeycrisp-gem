5aa39ec75072984d99ac7f6b9999f0dfe7aee6be
Christa Hartsock <christa@codeforamerica.org>
2019-05-16 11:09:05 -0700

    Remove GCF-specific assets and styles
    Also adds some generally useful styles and assets from the GCF codebase.
    
    Lastly, moves styleguide reference-specific styles out of the gem and
    serves the compiled CSS (rather than just the gem's CSS) on the
    reference page. As a result, the styleguide reference that is present in
    each project using the gem will now show what their styles should be
    (using their CSS and all overrides) rather than just what is in the gem.

--- spec/test_app/config/initializers/assets.rb
+++ spec/test_app/config/initializers/assets.rb
@@ -1,2 +1,2 @@
 Rails.application.config.assets.version = "1.0"
-Rails.application.config.assets.precompile += %w(emoji_pairs/* emojis/*)
+Rails.application.config.assets.precompile += %w(emoji_pairs/* emojis/* calfresh_logo.png)
