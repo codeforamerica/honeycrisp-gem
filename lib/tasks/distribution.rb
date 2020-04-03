db1ccef8854171ed56f4ac6c4f39d3c5a3d481c3
Ben Calegari <bcalegari@codeforamerica.org>
2020-04-03 16:23:47 -0700

    Replace css distribution generation with sprockets
    [#176]

--- lib/tasks/distribution.rb
+++ lib/tasks/distribution.rb
@@ -1,5 +1,4 @@
 require "fileutils"
-require "sass"
 require "sprockets_extension/uglifier_source_maps_compressor"
 
 class Distribution
@@ -18,12 +17,11 @@ class Distribution
   end
 
   def compile_css
-    Sass.load_paths << Dir.pwd + "/vendor/assets/stylesheets/" <<
-        Dir.pwd + "/tmp/bourbon" <<
-        Dir.pwd + "/tmp/neat"
     css_root = Dir.pwd + "/dist/css"
     FileUtils.mkdir_p(css_root)
-    Sass.compile_file("#{Dir.pwd}/app/assets/stylesheets/cfa_styleguide_main.scss", css_root + "/honeycrisp.css")
+    sprockets = create_sprockets_env
+    assets = sprockets.find_asset("#{Dir.pwd}/app/assets/stylesheets/cfa_styleguide_main.scss")
+    assets.write_to(css_root + "/honeycrisp.css")
   end
 
   def compile_js
@@ -46,20 +44,27 @@ class Distribution
   def create_sprockets_env(compress: false)
     Sprockets::Environment.new do |env|
       env.js_compressor = :uglify_with_source_maps if compress
+      env.css_compressor = :sass if compress
       env.append_path("#{Dir.pwd}/app/assets/javascripts/")
+      env.append_path("#{Dir.pwd}/app/assets/stylesheets/")
       env.append_path("#{Dir.pwd}/vendor/assets/javascripts/")
+      env.append_path("#{Dir.pwd}/vendor/assets/stylesheets/")
       env.append_path("#{jquery_path}/assets/javascripts/")
+      env.append_path("#{Dir.pwd}/tmp/bourbon")
+      env.append_path("#{Dir.pwd}/tmp/neat")
+      # Defining asset_path fixed the error in the main scss. ¯\_(ツ)_/¯
+      env.context_class.class_eval { def asset_path(path, options = {}); end }
     end
   end
+end
 
-  def install_dependencies
-    `bourbon install --path tmp && neat install`
-    FileUtils.move(Dir.pwd + "/neat", Dir.pwd + "/tmp/neat/")
+def install_dependencies
+  `bourbon install --path tmp && neat install`
+  FileUtils.move(Dir.pwd + "/neat", Dir.pwd + "/tmp/neat/")
 
-    Sprockets.register_compressor(
-        "application/javascript",
-        :uglify_with_source_maps,
-        UglifierSourceMapsCompressor,
-    )
-  end
+  Sprockets.register_compressor(
+    "application/javascript",
+      :uglify_with_source_maps,
+      UglifierSourceMapsCompressor,
+  )
 end
