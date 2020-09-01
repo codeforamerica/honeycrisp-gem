d354f2aceae5b821016ea341eaa9e7d6b54e1605
Laura Kogler <lkogler@codeforamerica.org>
2020-09-01 14:55:27 -0700

    Fix behavior for sidebar toggle and clean up code

--- app/assets/javascripts/sidebar_nav.js
+++ app/assets/javascripts/sidebar_nav.js
@@ -1,17 +1,9 @@
 //= require jquery
 $(document).ready(function() {
-    //collapse menu if its mobile and you click a link in menu sub items
     $('.sidebar-nav').on('click', 'a', function ()
     {
         if( $('.sidebar-collapse-toggle').is(':visible') ) {
-            $(".sidebar").css({
-                "width": "17em",
-                "display": "none"
-            });
-            $(".left-sidebar-slab-container").css({
-                "margin-left": "0",
-                "padding-left": "0"
-            });
+            collapseSidebar();
         }
     });
 
@@ -22,67 +14,25 @@ $(document).ready(function() {
     });
 });
 
-$(window).resize(function() {
-    //needed jquery resize for when someone changes screen size
-    //works if same size but gets caught in either mobile or desktop without this
-    if( $('.sidebar-collapse-toggle').first().is(':hidden') )
-    {
-        $('.sidebar-collapse-toggle').text('Close');
-        $(".sidebar").css({
-            "display" : "inline-block"
-        });
-        $(".sidebar-nav").css({
-            "width": "12em"
-        });
-        $(".left-sidebar-slab-container").css({
-            "margin-left": "11em",
-            "padding-left": "1em"
-        });
-    }
-    else
-    {
-        $('.sidebar-collapse-toggle').text('Menu');
-        $(".sidebar").css({
-            "width": "17em",
-            "display" : "none"
-        });
-
-        $(".left-sidebar-slab-container").css({
-            "margin-left": "0",
-            "padding-left": "0"
-        });
-    }
-});
-
 function toggleNav() {
     if($(".sidebar").is(":hidden"))
     {
-        $('.sidebar-collapse-toggle').text('Close');
-        $(".sidebar").css({
-            "display" : "inline-block"
-        });
-
-        $(".sidebar-nav").css({
-            "width": "100%"
-        });
-
-        $(".left-sidebar-slab-container").css({
-            "margin-left": "11em",
-            "padding-left": "1em"
-        });
+      openSidebar();
     }
     else
     {
-        $('.sidebar-collapse-toggle').text('Menu');
-        $(".sidebar").css({
-            "width": "17em",
-            "display" : "none"
-        });
-        $(".left-sidebar-slab-container").css({
-            "margin-left": "0",
-            "padding-left": "0"
-        });
+      collapseSidebar();
     }
+}
+
+function collapseSidebar() {
+  $('.sidebar-collapse-toggle').text('Menu');
+  $(".sidebar").removeClass("open");
+}
+
+function openSidebar() {
+  $('.sidebar-collapse-toggle').text('Close');
+  $(".sidebar").addClass("open");
 }
 
 function setSelectedState() {
