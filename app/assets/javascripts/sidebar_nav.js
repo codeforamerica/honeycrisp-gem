f70a6b9f2f532ff405868626829b7eddbdff7d1d
Symonne Singleton <symonne@codeforamerica.org>
2020-09-03 16:44:13 -0700

    Clean up jquery selectors in sidebar_nav.js
    Co-authored-by: Laura Kogler <lkogler@codeforamerica.org>

--- app/assets/javascripts/sidebar_nav.js
+++ app/assets/javascripts/sidebar_nav.js
@@ -36,20 +36,20 @@ function openSidebar() {
 }
 
 function setSelectedState() {
-  $('.sidebar-nav ul li .sidebar__sub-items li').removeClass("active");
+  $('.sidebar__sub-items li').removeClass("active");
 
   var url_array = window.location.toString().split("/");
   var page = url_array[url_array.length - 1].split("#")[0];
 
   if (page !== "styleguide") {
-    var sidebar_page_match = $('.sidebar-nav ul li .sidebar__sub-items li a[href*="' + page + '"]');
+    var sidebar_page_match = $('.sidebar__sub-items a[href*="' + page + '"]');
     if (sidebar_page_match.length) {
       sidebar_page_match.parent().addClass('active');
     }
   }
   else {
     if (window.location.hash) {
-      $('.sidebar-nav ul li .sidebar__sub-items li a[href*="' + window.location.hash + '"]').parent().addClass("active");
+      $('.sidebar__sub-items a[href*="' + window.location.hash + '"]').parent().addClass("active");
     }
   }
 }
