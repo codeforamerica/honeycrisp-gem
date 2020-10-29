ddf6867bb93e85310ec98a7e564bfdf1a9dcce6b
Christa Hartsock <christa@codeforamerica.org>
2020-10-29 13:30:44 -0700

    Allow for arbitrary method names on form example
    This allows us to use any method name prefixed with example_method as a
    method for a form, which will generate unique IDs. Unique IDs are
    necessary for doing realistic accessibility testing with Wave.
    
    [#175500754]
    
    Co-authored-by: Ben Sheldon <ben@codeforamerica.org>

--- app/models/cfa/styleguide/form_example.rb
+++ app/models/cfa/styleguide/form_example.rb
@@ -18,6 +18,16 @@ module Cfa
                     :none
 
       validates_presence_of :example_method_with_validation, message: "This is an example error message."
+
+      def method_missing(method_name, *args, **kwargs, &block)
+        return super unless method_name.to_s.starts_with?("example_method")
+
+        nil
+      end
+
+      def respond_to_missing?(method_name, *_args)
+        method_name.to_s.starts_with?("example_method")
+      end
     end
   end
 end
