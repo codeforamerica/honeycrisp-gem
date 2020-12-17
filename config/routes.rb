f7a3bc1bb0a7f899849dce1d0187764e64c45624
Ben Golder <bgolder@codeforamerica.org>
2020-12-17 11:55:29 -0800

    Add Honeycrisp Compact page and data table example - Remove superfluous class on navigation links
    Addresses Github issues #211 and #208
    Pivotal Tracker story [#176210053]

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Cfa::Styleguide::Engine.routes.draw do
   get "/styleguide/form-builder" => "pages#form_builder", as: :styleguide_form_builder
   get "/styleguide/examples/*example_path" => "examples#show", as: :styleguide_example
   get "/styleguide/emojis" => "pages#emojis", as: :styleguide_emojis
+  get "/styleguide/honeycrisp-compact" => "pages#honeycrisp_compact", as: :styleguide_honeycrisp_compact
 end
