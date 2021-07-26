8578a6434c2656f450fc8e19195b6f60de6c28f4
Colt Borg <cborg@codeforamerica.org>
2021-07-26 17:51:00 -0500

    Upgrade axe-matchers to axe-core-rspec
    [#267]
    
    Co-authored-by: Ben Golder <bgolder@codeforamerica.org>

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -11,34 +11,29 @@ GENERAL_ACCESSIBILITY_SKIPS = [
 ].freeze
 
 ## Examples with accessibility issues that must be fixed in the fullness of time
-FIXABLE_ACCESSIBILITY_EXAMPLES = [
+FAILING_ACCESSIBILITY_EXAMPLES = [
   "form_builder/v1/cfa_checkbox_set",
   "form_builder/v1/cfa_checkbox_set_with_none",
-  "form_builder/v1/cfa_date_select",
   "form_builder/v1/cfa_input_field",
-  "form_builder/v1/cfa_radio_set",
   "form_builder/v1/cfa_radio_set_with_follow_up",
   "form_builder/v1/cfa_range_field",
   "form_builder/v1/cfa_select",
-  "form_builder/v1/cfa_textarea",
-  "molecules/form_group",
-  "molecules/form_group_error_state",
   "molecules/incrementer",
-  "molecules/inline_input_group",
-  "molecules/searchbar",
-  "molecules/text_input_group",
-  "molecules/two_up_input_group",
+  # New issues coming from axe-core-rspec upgrade
+  "molecules/progress_step_bar",
+  "molecules/show_more",
+  "organisms/pagination",
 ].freeze
 
-RSpec.describe "Acessibility", js: true do
+RSpec.describe "Accessibility", js: true do
   each_example do |example, example_path|
     describe example do
       it "passes Axe matchers" do
-        pending("temporarily ignoring accessibility checks") if FIXABLE_ACCESSIBILITY_EXAMPLES.include?(example)
+        pending("temporarily ignoring accessibility checks: #{example}") if FAILING_ACCESSIBILITY_EXAMPLES.include?(example)
 
         visit example_path
 
-        expect(page).to be_accessible.skipping(*GENERAL_ACCESSIBILITY_SKIPS)
+        expect(page).to be_axe_clean.skipping(*GENERAL_ACCESSIBILITY_SKIPS).according_to :wcag2a
       end
     end
   end
