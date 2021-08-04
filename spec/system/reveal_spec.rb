3837fb79e98377acc5de7d21749e8dc6b82e0090
Colt Borg <cborg@codeforamerica.org>
2021-08-04 12:53:22 -0500

    Fix reveal and add tests
    - Closes #275

---
+++ spec/system/reveal_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe "Reveal" do
+  before do
+    visit "cfa/styleguide/examples/molecules/reveal"
+  end
+
+  describe "default view" do
+    it "on page load, trigger is visible and content is not", js: true do
+      expect(page).to have_content "Learn more about student eligibility"
+    end
+  end
+
+  describe "show reveal content" do
+    it "reveals content when the reveal link is clicked", js: true do
+      click_on "Learn more about student eligibility"
+
+      expect(page).to have_content "In order to qualify as a college student, you have to meet one of the following exemptions:"
+      expect(page).to have_content "Work an average at least 20 hours a week"
+    end
+  end
+
+  describe "hide reveal content" do
+    it "hides content when the reveal link is clicked with content already showing", js: true do
+      click_on "Learn more about student eligibility"
+      click_on "Learn more about student eligibility"
+
+      expect(page).not_to have_content "In order to qualify as a college student, you have to meet one of the following exemptions:"
+    end
+  end
+end
