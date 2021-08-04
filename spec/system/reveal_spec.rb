353a7f1d77c81108627723df1f7da201b04c6ba7
Colt Borg <cborg@codeforamerica.org>
2021-08-04 13:28:32 -0500

    Add more reveal assertions

--- spec/system/reveal_spec.rb
+++ spec/system/reveal_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 
+# TODO: refactor to smaller feature spec when we add Jest JavaScript unit tests
 describe "Reveal" do
   before do
     visit "cfa/styleguide/examples/molecules/reveal"
@@ -8,6 +9,7 @@ describe "Reveal" do
   describe "default view" do
     it "on page load, trigger is visible and content is not", js: true do
       expect(page).to have_content "Learn more about student eligibility"
+      expect(page).not_to have_content "In order to qualify as a college student, you have to meet one of the following exemptions:"
     end
   end
 
