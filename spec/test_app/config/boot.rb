018f7c5c77770628a9edc4a28004887e68c65ac5
Arin Choi <achoi@codeforamerica.org>
2025-07-31 10:38:39 -0700

    Require it in the boot.rb

--- spec/test_app/config/boot.rb
+++ spec/test_app/config/boot.rb
@@ -2,4 +2,6 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
 
 require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+require "logger" # Fix concurrent-ruby removing logger dependency which Rails itself does not have
+
 $LOAD_PATH.unshift File.expand_path("../../../lib", __dir__)
